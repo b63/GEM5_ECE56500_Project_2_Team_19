f602431cfd8a6f64ea5978749245475170dc1278
rabbit <rabbit@rabbits-MacBook-Air.local>
2023-12-02 22:56:17 -0500

    Increase maxInst to 5mil

--- script.py
+++ script.py
@@ -1,21 +1,21 @@
-import os
+import time
 import re
 from pprint import pprint
 import subprocess
 import argparse
 
-
+start = time.time()
 benchmarks = ['perlbench_s', 'gcc_s', 'bwaves_s','mcf_s', 'cactuBSSN_s', 'deepsjeng_s', 'lbm_s', 
-                'omnetpp_s', 'xalancbmk_s', 'specrand_is', 'specrand_fs', 'cam4_s', 'pop2_s', 
-                'imagick_s', 'nab_s', 'fotonik3d_s', 'roms_s', 'exchange2_s', 'xz_s', 
-                'perlbench', 'bzip2', 'gcc', 'bwaves', 'gamess', 'mcf', 'milc', 'zeusmp', 'gromacs', 
-                'cactusADM', 'leslie3d', 'namd', 'gobmk', 'dealII', 'soplex', 'povray', 'calculix', 
-                'hmmer', 'sjeng', 'GemsFDTD', 'libquantum', 'h264ref', 'tonto', 'lbm', 'omnetpp', 
-                'astar', 'wrf', 'sphinx3', 'xalancbmk', 'specrand_i' ,'specrand_f']
+              'omnetpp_s', 'xalancbmk_s', 'specrand_is', 'specrand_fs', 'cam4_s', 'pop2_s', 
+              'imagick_s', 'nab_s', 'fotonik3d_s', 'roms_s', 'exchange2_s', 'xz_s', 
+              'perlbench', 'bzip2', 'gcc', 'bwaves', 'gamess', 'mcf', 'milc', 'zeusmp', 'gromacs', 
+              'cactusADM', 'leslie3d', 'namd', 'gobmk', 'dealII', 'soplex', 'povray', 'calculix', 
+              'hmmer', 'sjeng', 'GemsFDTD', 'libquantum', 'h264ref', 'tonto', 'lbm', 'omnetpp', 
+              'astar', 'wrf', 'sphinx3', 'xalancbmk', 'specrand_i' ,'specrand_f']
 
 benchmarks_2k17 = ["bwaves_s", "cactuBSSN_s", "lbm_s", "cam4_s", "pop2_s", "imagick_s", "nab_s",
-                    "fotonik3d_s", "specrand_fs", "perlbench_s", "gcc_s", "mcf_s", "omnetpp_s",
-                    "xalancbmk_s", "deepsjeng_s", "exchange2_s", "xz_s", "specrand_is",]
+                   "fotonik3d_s", "specrand_fs", "perlbench_s", "gcc_s", "mcf_s", "omnetpp_s",
+                   "xalancbmk_s", "deepsjeng_s", "exchange2_s", "xz_s", "specrand_is",]
 data = {}
 
 parser = argparse.ArgumentParser(
@@ -42,7 +42,7 @@ if args.s17:
 for benchmark in benchmarks:
     print(f"Running {benchmark}...")    
     proc = subprocess.Popen(["./build/ECE565-X86/gem5.opt", "configs/spec/spec_se.py", "-b", benchmark, 
-    	   f"--cpu-type={args.cpu_type}", "--maxinsts=1000000", "--l1d_size=16kB", 
+    	   f"--cpu-type={args.cpu_type}", "--maxinsts=5000000", "--l1d_size=16kB", 
            "--l1i_size=16kB", "--l1d_assoc=2","--l1i_assoc=2",
            "--caches", "--l2cache", f"--l2_size={args.l2_size}", f"--l2_assoc={args.l2_assoc}"],
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -62,12 +62,5 @@ for benchmark in benchmarks:
 
 
 pprint(data)
-
-
-
-
-
-
-
-
-
+end = time.time()
+print(f"Total runtime: {end - start}")
