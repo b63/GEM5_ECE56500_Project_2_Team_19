ecdff04db46f2a43276f2ff6e442c07fb67d66aa
rabbit <rabbit@rabbits-MacBook-Air.local>
2023-12-08 15:56:52 -0500

    getVictim changed to OPT

--- src/mem/cache/replacement_policies/opt.cc
+++ src/mem/cache/replacement_policies/opt.cc
@@ -140,7 +140,7 @@ OPT::getVictim(const ReplacementCandidates& candidates) const
     std::string victim_addr_in_hex_str = int_to_hex_str(std::static_pointer_cast<OPTReplData>(victim->replacementData)->addr);
     unsigned int victim_last_access;
     if (trace.find(victim_addr_in_hex_str) != trace.end()){
-        std::vector<int> victim_mem_access = trace.find(victim_addr_in_hex_str).second;
+        std::vector<int> victim_mem_access = trace.find(victim_addr_in_hex_str)->second;
         victim_last_access = victim_mem_access[victim_mem_access.size()-1]; // Last element will show furthest away access
     }
     else
@@ -154,7 +154,7 @@ OPT::getVictim(const ReplacementCandidates& candidates) const
         DPRINTF(ReplacementOPT, "Looking at candidate with address %s \n", candidate_addr_hex_str);
         unsigned int candidate_last_access;
         if (trace.find(candidate_addr_hex_str) != trace.end()){
-            std::vector<int> mem_access = trace.find(candidate_addr_hex_str).second;
+            std::vector<int> mem_access = trace.find(candidate_addr_hex_str)->second;
             candidate_last_access = mem_access[mem_access.size()-1];
         }
         else
