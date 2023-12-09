bfd305553af16857d9deea85f6d10e28d2c07e10
rabbit <rabbit@rabbits-MacBook-Air.local>
2023-12-09 11:04:39 -0500

    getVictim changed to OPT

--- src/mem/cache/replacement_policies/opt.cc
+++ src/mem/cache/replacement_policies/opt.cc
@@ -142,7 +142,7 @@ OPT::getVictim(const ReplacementCandidates& candidates) const
     std::string victim_addr_in_hex_str = int_to_hex_str(std::static_pointer_cast<OPTReplData>(victim->replacementData)->addr);
     unsigned int victim_last_access = 0;
     DPRINTF(ReplacementOPT, "Looking at victim with address %s\n", victim_addr_in_hex_str);
-    ReplaceableEntry* speculative_victim;
+    ReplaceableEntry* speculative_victim = NULL;
     
     if(auto search = trace.find(victim_addr_in_hex_str); search != trace.end()){
         std::vector<unsigned> victim_mem_access = search->second;
